15ae4a515ad7af262bcb5ad268df7cec1a749a0b
AlexisEspinosaGayosso <Alexis.Espinosa@pawsey.org.au>
2018-11-19 12:23:29 +0800

    CFDEM Container
    Container is ready to run in Pawsey
    Example scripts for a tutorial case are given

---
+++ openfoam-4.x_CFDEM/tutorials_pawsey/cfdemSolverPiso/periodicChannel/parDEMrun.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#===================================================================#
+# DEMrun script for ErgunTestMPI testcase
+# init ErgunTestMPI
+# Christoph Goniva - July 2014
+#===================================================================#
+
+#- source CFDEM env vars
+#AEG:. ~/.bashrc
+
+#- include functions
+#AEG:source $CFDEM_SRC_DIR/lagrangian/cfdemParticle/etc/functions.sh
+source $SLURM_SUBMIT_DIR/functions.sh
+
+echo "starting DEM run in parallel..."
+#--------------------------------------------------------------------------------#
+#- define variables
+casePath="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
+logpath="$casePath"
+headerText="run_liggghts_init_DEM"
+logfileName="log_$headerText"
+solverName="in.liggghts_init"
+nrProcs=4
+machineFileName="none"   # yourMachinefileName | none
+debugMode="off"
+#--------------------------------------------------------------------------------#
+
+#- call function to run DEM case
+parDEMrun $logpath $logfileName $casePath $headerText $solverName $nrProcs $machineFileName $debugMode
+
